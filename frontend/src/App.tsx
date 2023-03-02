664c0849af4af522996243af7edf8d3172ffda8e
Tales Rodrigues <tales00001@gmail.com>
2023-03-02 16:06:30 -0300

    Reorganizing components II

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
 import React, { useContext } from 'react'
 import './App.css'
 
-import Header from './components/Header'
-import Organize from './components/Organize'
-import ToDoListSection from './components/ToDoListSection'
-import GoodThings from './components/GoodThings'
-import GetInTouch from './components/GetInTouch'
-import Footer from './components/Footer'
+import Header from './sections/Header'
+import Organize from './sections/Organize'
+import Lists from './sections/Lists'
+import GoodThings from './sections/GoodThings'
+import GetInTouch from './sections/GetInTouch'
+import Footer from './sections/Footer'
+
 import Login from './components/Login'
+
 import { ToDoContext } from './context'
 
 function App() {
    const { displayLogin } = useContext(ToDoContext)
-   console.log(displayLogin)
    
    return (
       <div id='app-container'>
          {displayLogin && <Login />}
          <Header />
          <Organize />
-         <ToDoListSection />
+         <Lists />
          <GoodThings />
          <GetInTouch />
          <Footer />
